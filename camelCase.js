9d2a199124783b0ea22abd5d87eb261b32a97110
khutija.tayyaba@facillima.com <khutija.tayyaba@facillima.com>
2024-05-27 09:45:15 +0200

    Added problem statement

--- camelCase.js
+++ camelCase.js
@@ -1,3 +1,11 @@
+/*
+Have the function camelCaseWithoutSpecialCharacters(str) take the str parameter being passed 
+and return it in proper camel case format where the first letter of each word is capitalized 
+(excluding the first letter). The string will only contain letters and some combination of delimiter 
+punctuation characters separating each word.
+
+For example: if str is "BOB loves-coding" then your program should return the string bobLovesCoding.
+*/
 
 function camelCaseWithoutSpecialCharacters(str)
 {
