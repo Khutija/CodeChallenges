e9b5573cad640dbdab0b4f5887f2b6d0aae29967
khutija.tayyaba@facillima.com <khutija.tayyaba@facillima.com>
2024-05-28 18:20:00 +0200

    binaryConverter

---
+++ binaryConverter.js
@@ -0,0 +1,19 @@
+/*Using the JavaScript language, have the function BinaryConverter(str) 
+return the decimal form of the binary value. For example:
+if 101 is passed return 5, or if 1000 is passed return 8. */
+function binaryConverter(str)
+{
+    let res = 0;
+    let arr = str.split("").reverse().join("");
+    for (let i = 0; i < arr.length; i++)
+    {
+
+        if (arr[i] == "1")
+        {
+            res += Math.pow(2, i);
+        }
+    }
+    return res;
+}
+
+console.log(binaryConverter("1000"));
